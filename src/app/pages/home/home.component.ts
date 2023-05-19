71de9453c7aab313e2b83fa76d71579f28bc303a
hsonnv2307@gmail.com <87641750+hoangson237@users.noreply.github.com>
2023-05-19 14:53:59 +0700

    update loading, signin, proflie, search, logout

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieApiServiceService } from 'src/app/service/movie-api-service.service';
 import { Title, Meta } from '@angular/platform-browser';
+import {  ElementRef } from '@angular/core';
+import {  ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,8 +10,10 @@ import { Title, Meta } from '@angular/platform-browser';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  loading: boolean = true; // Corrected property name
+  @ViewChild('rowposterContainer') rowposterContainer!: ElementRef;
 
+
+  loading: boolean = true; // Corrected property name
   constructor(private service: MovieApiServiceService, private title: Title, private meta: Meta) {
     this.title.setTitle('Netflix - Watch Movie');
     this.meta.updateTag({ name: 'description', content: 'Wtch Online Movies' });
@@ -28,6 +32,10 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.loadData();
   }
+  
+  onRowPosterMouseEnter(): void {
+    this.rowposterContainer.nativeElement.scrollLeft += 100; // Thay đổi giá trị 100 thành giá trị cuộn mong muốn
+  }
 
   loadData(): void {
     this.loading = true; // Set loading to true before making API calls
@@ -35,35 +43,43 @@ export class HomeComponent implements OnInit {
     this.service.bannerApiData().subscribe((result) => {
       console.log(result, 'bannerresult#');
       this.bannerResult = result.results;
+      this.loading = false; // Set loading to false after data is loaded
     });
 
     this.service.trendingMovieApiData().subscribe((result) => {
       console.log(result, 'trendingresult#');
       this.trendingMovieResult = result.results;
+      this.loading = false; // Set loading to false after data is loaded
     });
 
     this.service.fetchActionMovies().subscribe((result) => {
       this.actionMovieResult = result.results;
+      this.loading = false; // Set loading to false after data is loaded
     });
 
     this.service.fetchAdventureMovies().subscribe((result) => {
       this.adventureMovieResult = result.results;
+      this.loading = false; // Set loading to false after data is loaded
     });
 
     this.service.fetchAnimationMovies().subscribe((result) => {
       this.animationMovieResult = result.results;
+      this.loading = false; // Set loading to false after data is loaded
     });
 
     this.service.fetchComedyMovies().subscribe((result) => {
       this.comedyMovieResult = result.results;
+      this.loading = false; // Set loading to false after data is loaded
     });
 
     this.service.fetchDocumentaryMovies().subscribe((result) => {
       this.documentaryMovieResult = result.results;
+      this.loading = false; // Set loading to false after data is loaded
     });
 
     this.service.fetchScienceFictionMovies().subscribe((result) => {
       this.sciencefictionMovieResult = result.results;
+      this.loading = false; // Set loading to false after data is loaded
     });
 
     this.service.fetchThrillerMovies().subscribe((result) => {
